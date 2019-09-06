4a4d6d3af9247ac9e523af69349840e9a3779400
Joshua Evans <TheJoshuaEvans@gmail.com>
2019-09-06 15:45:59 -0700

    Use-Specific Changes - Basic auth only works for specific endpoints - Added brute force attack protections - Create special "check-auth" endpoint

--- basic-auth.php
+++ basic-auth.php
@@ -4,17 +4,26 @@
  * Description: Basic Authentication handler for the JSON API, used for development and debugging purposes
  * Author: WordPress API Team
  * Author URI: https://github.com/WP-API
- * Version: 0.1
+ * Version: 0.2
  * Plugin URI: https://github.com/WP-API/Basic-Auth
  */
-
 function json_basic_auth_handler( $user ) {
 	global $wp_json_basic_auth_error;
+	$authenticating_users_key_prefix = "authenticating_users_cache_group";
+	$authenticating_users_transient_expiration = 2;
+	$allowed_uris = array(
+		'/wp-json/basic-auth/v1/check-auth'
+	);
 
 	$wp_json_basic_auth_error = null;
 
+	// Only use this authentication method on the correct paths
+	if ( !in_array($_SERVER['REQUEST_URI'], $allowed_uris) ) {
+		return $user;
+	}
+
 	// Don't authenticate twice
-	if ( ! empty( $user ) ) {
+	if ( !empty( $user ) ) {
 		return $user;
 	}
 
@@ -25,6 +34,16 @@ function json_basic_auth_handler( $user ) {
 
 	$username = $_SERVER['PHP_AUTH_USER'];
 	$password = $_SERVER['PHP_AUTH_PW'];
+
+	// Use transient storage to make sure the same user can't try to authenticate if they are already authenticating
+	$transient_username_key = $authenticating_users_key_prefix . $username;
+
+	if (false !== get_transient( $transient_username_key ) ) {
+		$wp_json_basic_auth_error = new WP_Error( 'already_processing_user_auth', 'Authentication for this user is already being processed' );
+		return null;
+	}
+
+	set_transient( $transient_username_key, true, $authenticating_users_transient_expiration );
 
 	/**
 	 * In multi-site, wp_authenticate_spam_check filter is run on authentication. This filter calls
@@ -39,11 +58,18 @@ function json_basic_auth_handler( $user ) {
 	add_filter( 'determine_current_user', 'json_basic_auth_handler', 20 );
 
 	if ( is_wp_error( $user ) ) {
+		/**
+		 * In case of an authentication error, delay the response for a couple of seconds (helping to prevent brute force
+		 * password guessing attacks)
+		 */
+		sleep( $authenticating_users_transient_expiration );
+
 		$wp_json_basic_auth_error = $user;
 		return null;
 	}
 
 	$wp_json_basic_auth_error = true;
+	delete_transient( $transient_username_key );
 
 	return $user->ID;
 }
@@ -60,3 +86,17 @@ function json_basic_auth_error( $error ) {
 	return $wp_json_basic_auth_error;
 }
 add_filter( 'rest_authentication_errors', 'json_basic_auth_error' );
+
+function return_completed_auth_json() {
+	$successObj->code = "authentication_success";
+	$successObj->message = "Authentication succeeded!";
+	return $successObj;
+}
+
+add_action( 'rest_api_init', function () {
+	// Very basic route that can be used to just check if a password is correct
+	register_rest_route( 'basic-auth/v1', '/check-auth', array(
+		'methods' => 'GET',
+		'callback' => 'return_completed_auth_json'
+	) );
+} );
